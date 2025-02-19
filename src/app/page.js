d28a2d7d62c9dddf7c990ec564e5599e83c5bee9
Nazia98175 <naziasiddiqui98175@gmail.com>
2025-02-19 20:49:22 +0530

    correction

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div className=" grid grid-cols-1 lg:grid-cols-2 min-h-screen bg-white">
-        <div className=" items-center max-w-full min-h-screen lg:flex invisible lg:visible justify-center hidden lg:flex ">
+        <div className=" items-center max-w-full min-h-screen invisible lg:visible justify-center hidden lg:flex ">
           <LoginIllustration className="" />
         </div>
         <Login />
